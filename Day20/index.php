62cd1fa43fbaeadce496f67db5de709333e00c85
Oleraj Hossin <52496066+Oleraj09@users.noreply.github.com>
2022-10-26 12:31:59 +0600

    Update index.php

--- Day20/index.php
+++ Day20/index.php
@@ -4,7 +4,7 @@ use App\classes\function1;
 use App\classes\menu;
 
 $index_obj = new function1();
-// $index_obj->redirect();
+$index_obj->redirect();
 $idx = new menu();
 echo "<pre>";
 print_r($idx->menu());
